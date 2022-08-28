474c970f86dfb8fc8a869c33aa2c04c80a38b11b
Gorilla Jones <kgorski90lol@gmail.com>
2022-08-28 15:02:22 -0500

    Fixed small problem with button script not displaying correct class

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -9,16 +9,22 @@ document.querySelectorAll(".carl").forEach(carl => {
     ${ btnHtml.join("") }
     </div>`);
     
-    const btn = carl.querySelectorAll(".carlButton");
-    // unselects all items.
-    btn.forEach((btn, i) =>{
+    const btns = carl.querySelectorAll(".carlButton");
+
+    btns.forEach((btn, i) => {
+
         btn.addEventListener("click", () => {
-            items.forEach(item => item.classList.remove("carlItem--selected"));
-            btn.classList.remove("carlButton--selected");
+            // unselects all items.
+            items.forEach(item => item.classList.remove("carlItem--selected")
+            );
+            btns.forEach(btn => btn.classList.remove("carlButton--selected")
+            );
+
             items[i].classList.add("carlItem--selected");
+
             btn.classList.add("carlButton--selected");
         });
     });
     items[0].classList.add("carlItem--selected");
-    btn[0].classList.add("carlButton--selected")
+    btns[0].classList.add("carlButton--selected")
 });
